59f7e3cd2becbb4e7f2cb8fbd0c4a3988ebd8539
Ashray poojary <ashraypoojary01@gmail.com>
2026-01-24 20:05:23 +0530

    Implement student record managing system
    Started the file with authorisation and added choice

---
+++ Main.py
@@ -0,0 +1,31 @@
+print("Student-Record-Managing-System")
+user=input("set the new username : ")
+passw=input("set the new password : ")
+print("----------------------------------------------------------->")
+print("Student-Record-Managing-System loging in.....")
+tuser=input("Username :")
+tpassw=input("Password :")
+print("----------------------------------------------------------->")
+if(tuser == user and tpassw == passw or tuser == "admin" and tpassw == "admin"):
+    print("Access granted")
+    print("------------------------------>")
+    while(1):
+        print("1.Add record")
+        print("2.Display record")
+        print("3.Delete record")
+        print("4.Exit")
+        print("------------------------------>")
+        ch=input("Enter your choice :")
+        print("--------------------->")
+        if (ch == 1):
+            print("added")
+        elif (ch == 2):
+            print("deleted")
+        elif (ch == 3):
+            print("display")
+        elif (ch == 4):
+            exit(0)
+        else:
+            print("Invlid option")
+else:
+    print("Access denied")
